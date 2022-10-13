94ddf92bea0430bb3120633aac250da0bde291ef
lauraecastellanos42 <100247144+lauraecastellanos42@users.noreply.github.com>
2022-10-12 21:09:49 -0500

    base para el proyecto colegio

---
+++ Colegio/src/test/java/com/Proyecto/Colegio/ColegioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.Colegio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColegioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
